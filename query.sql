3a5410d0497f086e74d5d70d4a3f8b3ab3aa8f24
Erica <erica@ELU-MBPro.local>
2018-12-20 17:07:59 -0600

    updates

--- query.sql
+++ query.sql
@@ -1,7 +1,9 @@
 -- example join statements
 -- need to join on both store and date
-select * from sales s
-join left
-features f on (s.store = f.store)
-join left
-stores st on (f.store = st.store);
+select * 
+from sales s1
+join 
+features f on (s1.store = f.store AND s1.date = f.date)
+join 
+stores s2 on (f.store = s2.store);
+
